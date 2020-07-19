d2b9fd3c678fd6174006d06cba75878754d6cec5
light8reeze <light8reeze@gmail.com>
2020-07-19 22:35:28 +0900

    Initial commit

---
+++ Src/Inc/DBSystem.hpp
@@ -0,0 +1,21 @@
+/**
+	@file	DBSystem.h
+	@brief	LightORM Project's DB module header
+	@date	2020-07-19
+	@auther light8reeze(light8reeze@gmail.com)
+*/
+#pragma once
+
+namespace LightORM
+{
+	/**
+		@brief	        DB system interface
+		@details		
+		@date	        2020-07-19
+		@auther         light8reeze(light8reeze@gmail.com)
+	*/
+	class IDBSystem
+	{
+
+	};
+}
---
+++ Src/Inc/LightORM.hpp
@@ -0,0 +1,20 @@
+/**
+	@file	LightORM.h
+	@brief	LightORM Project's macro header
+	@date	2020-07-19
+	@auther light8reeze(light8reeze@gmail.com)
+*/
+#pragma once
+
+/**
+	@brief	Platform definition Macro
+	@date	2020-07-19
+*/
+#define LORM_WINDOW
+#define LORM_LINUX
+
+/**
+	@brief	Develop mode Macro
+	@date	2020-07-19
+*/
+#define LORM_DEVELOP_MODE
